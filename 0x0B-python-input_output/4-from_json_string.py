6a132c97185b77a766866453010e742c9137758b
Ingenious464 <sincerityisgood@gmail.com>
2023-04-13 17:07:29 +0000

    task 3-6

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 6-from_json_string.py
+"""From JSON string to-object"""
+import json
+
+
+def from_json_string(my_str):
+    """Return an object (Python data structure) object represented by  a JSON string"""
+    return json.loads(my_str)
