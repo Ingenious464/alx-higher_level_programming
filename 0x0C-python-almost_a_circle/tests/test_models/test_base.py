e56e20d2323bdf511df173c8f6cada7d08cdf6cf
Ingenious464 <sincerityisgood@gmail.com>
2023-04-16 16:29:24 +0000

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,6 +6,9 @@ import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+import models.rectangle
+import models.square
+import models.base
 
 
 class TestBase_instantiation(unittest.TestCase):
@@ -27,3 +30,22 @@ class TestBase_instantiation(unittest.TestCase):
         b2 = Base(None)
         self.assertEqual(b1.id, b2.id - 1)
 
+class TestBase_to_json_string(unittest.TestCase):
+    """Unittests for testing to_json_string method of Base class"""
+
+    def test_to_json_string_rectangle_type(self):
+        r = Rectangle(10, 7, 2, 8, 6)
+        self.assertEqual(str, type(Base.to_json_string([r.to_dictionary()])))
+
+    def test_to_json_string_rectangle_one_dict(self):
+        r = Rectangle(10, 7, 2, 8, 6)
+        self.assertTrue(len(Base.to_json_string([r.to_dictionary()])) == 53)
+
+    def test_to_json_string_rectangle_two_dicts(self):
+        r1 = Rectangle(2, 3, 5, 19, 2)
+        r2 = Rectangle(4, 2, 4, 1, 12)
+        list_dicts = [r1.to_dictionary(), r2.to_dictionary()]
+        self.assertTrue(len(Base.to_json_string(list_dicts)) == 106)
+
+if __name__ == "__main__":
+    unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -7,11 +7,45 @@ import sys
 import unittest
 from models.base import Base
 from models.square import Square
+import models.rectangle
+import models.square
+import models.base
 
 
-class TestSquare_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Square class."""
+class TestSquare(unittest.TestCase):
+    """Class for all the tests"""
 
-    def test_is_base(self):
-        self.assertIsInstance(Square(10), Base)
+    def test_square(self):
+        """All the tests"""
+        a = Square(2)
+        self.assertEqual(a.size, 2)
+        self.assertEqual(a.x, 0)
+        self.assertEqual(a.y, 0)
+        self.assertEqual(a.id, 5)
+        b = Square(3, 4, 5)
+        self.assertEqual(b.size, 3)
+        self.assertEqual(b.x, 4)
+        self.assertEqual(b.y, 5)
+        self.assertEqual(b.id, 6)
+        c = Square(6, 5, 4, 12)
+        self.assertEqual(c.size, 6)
+        self.assertEqual(c.x, 5)
+        self.assertEqual(c.y, 4)
+        self.assertEqual(c.id, 12)
 
+        with self.assertRaises(TypeError, msg='width must be an integer'):
+            w = Square("i", 6)
+        with self.assertRaises(ValueError, msg='width must be > 0'):
+            o = Square(-2, 4)
+        with self.assertRaises(TypeError, msg='x must be an integer'):
+            r = Square(3, "e")
+        with self.assertRaises(ValueError, msg='x must be > 0'):
+            l = Square(2, 4, -8)
+        with self.assertRaises(TypeError, msg='y must be an integer'):
+            d = Square(5, 8, set([1, 2, 3, 4]))
+        with self.assertRaises(ValueError, msg='y must be > 0'):
+            s = Square(6, 7, -4)
+
+        a = Square(5, 4, 3, 2)
+        s = "[Square] (2) 4/3 - 5"
+        self.assertEqual(str(a), s)
